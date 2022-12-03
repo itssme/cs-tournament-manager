b9bd3ab07ebd686570ca151286f94cdc945e8d30
Joel Klimont <joel.klimont@gmail.com>
2022-12-03 21:43:09 +0100

    started working on elo ratings

--- app/elo.py
+++ app/elo.py
@@ -7,25 +7,35 @@ def expected_result(loc, aw):
     return [np.round(we, 3), 1 - np.round(we, 3)]
 
 
-def actual_result(loc, aw):
-    if loc < aw:
+def actual_result(team1_score, team2_score):
+    if team1_score < team2_score:
         wa = 1
         wl = 0
-    elif loc > aw:
+    elif team1_score > team2_score:
         wa = 0
         wl = 1
-    elif loc == aw:
+    else:  # team1_score == team2_score
         wa = 0.5
         wl = 0.5
     return [wl, wa]
 
 
-def calculate_elo(elo_l, elo_v, local_goals, away_goals):
-    k = 10  # TODO: balance
-    wl, wv = actual_result(local_goals, away_goals)
-    wel, wev = expected_result(elo_l, elo_v)
+def calculate_elo(team1_elo, team2_elo, team1_score, team2_score):
+    k = 200  # TODO: balance
+    wl, wv = actual_result(team1_score, team2_score)
+    wel, wev = expected_result(team1_elo, team2_elo)
 
-    elo_ln = elo_l + k * (wl - wel)
-    elo_vn = elo_v + k * (wv - wev)
+    elo_ln = team1_elo + k * (wl - wel)
+    elo_vn = team2_elo + k * (wv - wev)
 
     return elo_ln, elo_vn
+
+
+def main():
+    teams = [1000, 2000, 1500, 800]
+
+    print(calculate_elo(1200, 2000, 16, 20))
+
+
+if __name__ == '__main__':
+    main()
